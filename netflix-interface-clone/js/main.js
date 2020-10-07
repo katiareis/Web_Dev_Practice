91f10fd0f56ee2123655b0a4e1f4224f48c5f3fa
Katia Reis <reis.katia86@gmail.com>
2020-10-07 12:52:01 -0300

    Recriando a Interface do Netflix
    Recriando a interface do principal site de streaming mundial utilizando tecnologias simples como HTML5, CSS3 e JavaScript. Nesse projeto: como estruturar um layout, técnicas de CSS3 com containers e variáveis, como posicionar os elementos com Flexbox e como utilizar plugins Jquery a favor da sua aplicação.

---
+++ netflix-interface-clone/js/main.js
@@ -0,0 +1,17 @@
+//Set UP do Owl
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
